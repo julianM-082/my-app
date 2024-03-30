b3eef95fb89968a61120ca995c6d4494823f874c
julianM-082 <jmolinaruiz10@gmail.com>
2024-03-29 20:54:39 -0500

    Aplicar estilos de css a los componentes

---
+++ src/Task.jsx
@@ -0,0 +1,10 @@
+export function Task() {
+    return <div style={{
+        backgroundColor: "red",
+        color: "#FFF",
+        padding: '20px'
+    }}>
+        <h1 style={{fontWeight : ""}}>Mi primer tarea</h1>
+        <p>Tarea Realizada</p>
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -3,29 +3,12 @@ import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { NavBar } from "./Product";
 import { Button } from "./Button";
+import { Task } from "./Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <>
-        <Button text= "Click me"/>
-        <Button text = "Pay"/>
-        <Button text = "3000"/>
-        {/* <UserCard
-            name="Carlos"
-            amount={1000}
-            married={true}
-            points = {[10, 20, 30, 40]} 
-            address={{ street: "123 Street", city: "New York" }}
-            greet = {()=>{alert('hello')}}
-        />
-
-        <UserCard
-            name= "Juan Carlos"
-            amount= {1000}
-            married= {false}
-            address={{street: "juan Street", city: "Miami"}}
-            points = {[10,20]}
-        /> */}
+        <Task/>
     </>
 );
