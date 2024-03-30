ff372ca8217e555a989988307c68e522c729da22
julianM-082 <jmolinaruiz10@gmail.com>
2024-03-29 20:40:37 -0500

    Componente de button

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { NavBar } from "./Product";
+import { Button } from "./Button";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <>
-        <UserCard
+        <Button text= "Click me"/>
+        <Button text = "Pay"/>
+        <Button text = "3000"/>
+        {/* <UserCard
             name="Carlos"
             amount={1000}
             married={true}
-            points = {[10, 20, 30, 40]}
+            points = {[10, 20, 30, 40]} 
             address={{ street: "123 Street", city: "New York" }}
             greet = {()=>{alert('hello')}}
         />
@@ -22,6 +26,6 @@ root.render(
             married= {false}
             address={{street: "juan Street", city: "Miami"}}
             points = {[10,20]}
-        />
+        /> */}
     </>
 );
