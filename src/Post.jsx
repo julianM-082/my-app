84b92ebc9d3da4f7e5ebb9c49cd9c4829e072317
julianM-082 <jmolinaruiz10@gmail.com>
2024-03-31 20:16:42 -0500

    Hooks

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,9 +1,13 @@
+import {VscBug} from 'react-icons/vsc'
+
 export function Post(){
     return <button onClick={()=>{
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response=>response.json())
         .then(data => console.log(data))
     }}>
+        <VscBug/>
         Taer Datos
     </button>
 }
+
--- src/index.js
+++ src/index.js
@@ -8,11 +8,21 @@ import { Button } from "./Button";
 import { Task } from "./Task";
 import { Saludar } from "./Saludar";
 import { Post } from "./Post";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Counter() {
+    let counter = 0;
+
+    return <div>
+        <h1>Counter: {counter}</h1>
+        <button>Sumar</button>
+    </div>
+}
 
 root.render(
     <>
-    <Post/>
+    <Counter/>
     </>
 
 );
